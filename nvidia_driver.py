1b5023cb025df8d4691d8f5a6f74645834c74edd
Giann1s <giannislag@proton.me>
2022-10-16 17:10:03 +0300

    Use New Feature Branch instead of Production Branch  and other improvments

--- nvidia_driver.py
+++ nvidia_driver.py
@@ -9,33 +9,38 @@ from bs4 import BeautifulSoup
 
 if "--url" in sys.argv or "--filename" in sys.argv:
 
-    def get_filename(url):
+    def get_text(text: str, start: str, end: str) -> str:
+        start_idx = text.find(start) + len(start)
+        end_idx = text.find(end, start_idx)
+        return text[start_idx:end_idx]
+        
+    def get_filename(url: str) -> str:
 
-        last_slash_position = 0 
+        last_slash_pos = 0 
 
         for i in range(len(url)):
             if url[i] == "/":
-                last_slash_position = i
+                last_slash_pos = i
         
-        return url[last_slash_position + 1:]
+        return url[last_slash_pos + 1:]
 
     unix_drivers = "https://www.nvidia.com/en-us/drivers/unix/"
 
-    result1 = requests.get(unix_drivers)
-    page1 = BeautifulSoup(result1.text, "html.parser")
-    url = page1.find(text=" Latest Production Branch Version: ").parent.find("a").get("href")
+    result = requests.get(unix_drivers)
+    page = BeautifulSoup(result.text, "html.parser")
+    p_tag = str(page.find(text="Linux x86_64/AMD64/EM64T").parent.parent)
+    url = get_text(p_tag, ' Latest New Feature Branch Version: <a href="', '">')
 
-    result2 = requests.get(url)
-    page2 = BeautifulSoup(result2.text, "html.parser")
-    url = f"""https://www.nvidia.com{page2.find(id="lnkDwnldBtn").get("href")}"""
+    result = requests.get(url)
+    page = BeautifulSoup(result.text, "html.parser")
+    url = f"""https://www.nvidia.com{page.find(id="lnkDwnldBtn").get("href")}"""
+    url = get_text(url, 'url=', '&')
 
-    start = url.find("url=") + 4
-    end = url.find(".run") + 4
-    driver_url = f"https://us.download.nvidia.com{url[start:end]}"
+    url = f"https://us.download.nvidia.com{url}"
 
-    driver_filename = get_filename(driver_url)
-
-    if "--url" in sys.argv:
-        print(driver_url)
+    filename = get_filename(url)
+    
     if "--filename" in sys.argv:
-        print(driver_filename)
+        print(filename)
+    if "--url" in sys.argv:
+        print(url)

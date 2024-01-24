25abe17739069d0056bc16383cf77062e452e42e
Giannis Lagodimos <giannislag@proton.me>
2024-01-24 19:05:45 +0200

    Don't use deprecated find() argument and minor changes

--- nvidia_driver.py
+++ nvidia_driver.py
@@ -6,29 +6,30 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
+def get_text(text: str, start: str, end: str) -> str:
+    """Returns the string found between first "start" and "end" strings."""
+
+    start_idx = text.find(start) + len(start)
+    end_idx = text.find(end, start_idx)
+    return text[start_idx:end_idx]
+
+def get_filename(url: str) -> str:
+
+    last_slash_pos = 0
+
+    for i in range(len(url)):
+        if url[i] == "/":
+            last_slash_pos = i
+
+    return url[last_slash_pos + 1:]
+
 if "--url" in sys.argv or "--filename" in sys.argv:
-
-    def get_text(text: str, start: str, end: str) -> str:
-        """Returns the string found between first "start" and "end" strings."""
-        start_idx = text.find(start) + len(start)
-        end_idx = text.find(end, start_idx)
-        return text[start_idx:end_idx]
-
-    def get_filename(url: str) -> str:
-
-        last_slash_pos = 0
-
-        for i in range(len(url)):
-            if url[i] == "/":
-                last_slash_pos = i
-
-        return url[last_slash_pos + 1:]
 
     unix_drivers = "https://www.nvidia.com/en-us/drivers/unix/"
 
     result = requests.get(unix_drivers)
     page = BeautifulSoup(result.text, "html.parser")
-    p_tag = str(page.find(text="Linux x86_64/AMD64/EM64T").parent.parent)
+    p_tag = str(page.find(string="Linux x86_64/AMD64/EM64T").parent.parent)
     url = get_text(p_tag, ' Latest Production Branch Version: <a href="', '">')
 
     result = requests.get(url)
